6b569c73edc3c20642009a7ab1272ec489ef44c7
Brandon Shin <bmshin02@gmail.com>
2024-01-21 00:17:38 -0800

    Fixes #1 - scooter movement
    - turning
    - forward and back

--- UCR-Gen-Zooom/Assets/Scripts/scooterMovement.cs
+++ UCR-Gen-Zooom/Assets/Scripts/scooterMovement.cs
@@ -1,72 +1,143 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class scooterMovement : MonoBehaviour
 {
-    [SerializeField] float motorTorque, brakeTorque, maxSteeringAngle, maxSpeed, motorcycleRotation;
+    private Rigidbody RB;
+    public GameObject CarBody;
+    public GameObject VisualBody;
 
-    [SerializeField] WheelCollider frontWheelCollider, backWheelCollider;
+    public Transform Wheel1;
+    public Transform Wheel2;
+    public Transform Wheel3;
+    public Transform Wheel4;
 
-    Rigidbody rb;
+    public Transform Right;
+    public Transform Left;
+    public Transform Straight;
 
-    float steeringAngle = 0;
-    public static bool isAccelerating, isBraking, isTurningLeft, isTurningRight, isDoingStop;
-    void Start()
+    public TrailRenderer Trail1;
+    public TrailRenderer Trail2;
+    public TrailRenderer Trail3;
+    public TrailRenderer Trail4;
+
+    public AudioSource CarEngine;
+    public AudioSource CarDrift;
+    public bool DriftCheck;
+
+    private float angleZ;
+
+    private void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        RB = gameObject.GetComponent<Rigidbody>();
     }
 
-    void Update()
+    private void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        if (verticalInput == 0 && horizontalInput ==0 )
-        {
-            rb.velocity = Vector3.zero;
-        }
 
-        //if vertical input is positive (w, up, forward), the scooter accelerates
-        if (verticalInput > 0)
+        angleZ = VisualBody.transform.localRotation.z;
+        Debug.LogWarning(angleZ);
+        if (Input.GetKey("w"))
         {
-            backWheelCollider.motorTorque = Mathf.Clamp(motorTorque, -maxSpeed, maxSpeed);
-        } 
-        else 
-        {
-            backWheelCollider.motorTorque = 0;
-        }
+            RB.velocity += transform.forward * 150 * Time.deltaTime;
 
-        //if vertical input is negative (s, down, backwards), the scooter brakes
-        if (verticalInput < 0)
-        {
-            Debug.LogWarning("BRAKE");
-            frontWheelCollider.brakeTorque = 0;
-            backWheelCollider.brakeTorque = 0;
-            
-        }
+            Wheel1.Rotate(0, 0, -500 * Time.deltaTime);
+            Wheel2.Rotate(0, 0, -500 * Time.deltaTime);
+            Wheel3.Rotate(0, 0, 500 * Time.deltaTime);
+            Wheel4.Rotate(0, 0, 500 * Time.deltaTime);
 
-       
-
-        //if horizontal input is negative (left), the scooter goes left
-        if (horizontalInput < 0)
-        {
-            steeringAngle = -maxSteeringAngle;
-        }
-        //if horizontal input is positive (right), the scooter goes right
-        else if (horizontalInput > 0) {
-            steeringAngle = maxSteeringAngle;
-        } else
-        {
-            steeringAngle = 0;
-        }
-
-        frontWheelCollider.steerAngle = steeringAngle;
-        if (isDoingStop)
-        {
-            motorcycleRotation += .0001f;
-            transform.Rotate(motorcycleRotation, 0, 0);
+            /*if (CarEngine.isPlaying == false)
+            {
+                CarEngine.Play();
+            }*/
         }
         else
         {
-            transform.rotation = Quaternion.identity;
+           /* CarEngine.Stop();*/
         }
+
+        if (Input.GetKey("s"))
+        {
+            RB.velocity -= transform.forward * 80 * Time.deltaTime;
+
+            Wheel1.Rotate(0, 0, 500 * Time.deltaTime);
+            Wheel2.Rotate(0, 0, 500 * Time.deltaTime);
+            Wheel3.Rotate(0, 0, -500 * Time.deltaTime);
+            Wheel4.Rotate(0, 0, -500 * Time.deltaTime);
+        }
+
+        if (Input.GetKey("a") && Input.GetKey("w"))
+        {
+            if (angleZ <= .2)
+            {
+                VisualBody.transform.Rotate(Vector3.forward * Time.deltaTime * 100);
+            }
+            
+
+            transform.Rotate(0, -30 * Time.deltaTime, 0);
+            CarBody.transform.rotation = Quaternion.Lerp(CarBody.transform.rotation, Left.rotation, 4 * Time.deltaTime);
+            RB.velocity += CarBody.transform.forward * 120 * Time.deltaTime;
+            RB.velocity -= transform.forward * 110 * Time.deltaTime;
+        }
+
+        if (Input.GetKey("d") && Input.GetKey("w"))
+        {
+            if (angleZ >= -.2)
+            {
+                VisualBody.transform.Rotate(Vector3.back * Time.deltaTime * 100);
+            }
+            
+            transform.Rotate(0, 30 * Time.deltaTime, 0);
+            CarBody.transform.rotation = Quaternion.Lerp(CarBody.transform.rotation, Right.rotation, 4 * Time.deltaTime);
+            RB.velocity += CarBody.transform.forward * 120 * Time.deltaTime;
+            RB.velocity -= transform.forward * 110 * Time.deltaTime;
+        }
+
+        if (!Input.GetKey("d") && !Input.GetKey("a"))
+        {
+            CarBody.transform.rotation = Quaternion.Lerp(CarBody.transform.rotation, Straight.rotation, 4 * Time.deltaTime);
+        }
+
+        if (angleZ < 0)
+        {
+            VisualBody.transform.Rotate(Vector3.forward * Time.deltaTime * 50);
+        }
+        if (angleZ > 0)
+        {
+            VisualBody.transform.Rotate(Vector3.back * Time.deltaTime * 50);
+        }
+
+        /*        if (angleZ > 0)
+                {
+                    VisualBody.transform.Rotate(Vector3.back * Time.deltaTime * 20);
+                }
+                if (angleZ < 0)
+                {
+                    VisualBody.transform.Rotate(Vector3.forward * Time.deltaTime * 20);
+                }*/
+
+        /*if (CarBody.transform.localRotation.y * 100 > 15 || CarBody.transform.localRotation.y * 100 < -15)
+        {
+            Trail1.emitting = true;
+            Trail2.emitting = true;
+            Trail3.emitting = true;
+            Trail4.emitting = true;
+
+            if (DriftCheck == false)
+            {
+                DriftCheck = true;
+                CarDrift.Play();
+            }
+        }
+        else
+        {
+            DriftCheck = false;
+
+            Trail1.emitting = false;
+            Trail2.emitting = false;
+            Trail3.emitting = false;
+            Trail4.emitting = false;
+        }*/
     }
 }
